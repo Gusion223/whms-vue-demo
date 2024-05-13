a848b2a7c3949daefe33eedc1e628cc80aaaace8
Kata Gusion <1511684358@qq.com>
2024-05-13 17:59:28 +0800

    修改basic-aside并加入了一些UI变化

--- src/pages/login/whms-basic-aside.vue
+++ src/pages/login/whms-basic-aside.vue
@@ -1,12 +1,13 @@
 <template>
   <el-scrollbar>
-    <el-menu :default-openeds="['1']">
+    <el-menu :default-openeds="['1']" :collapse="isCollapse">
       <!-- default-openeds表示默认打开什么 -->
       <el-sub-menu index="1">
         <template #title>
           <el-icon>
             <home-filled />
-          </el-icon>主页
+          </el-icon>
+          <span>主页</span>
         </template>
         <el-menu-item-group>
           <el-menu-item index="1-1">选项1</el-menu-item>
@@ -19,9 +20,7 @@
 
       <el-sub-menu index="2">
         <template #title>
-          <el-icon>
-            <Menu />
-          </el-icon>菜单
+          <el-icon><Menu /></el-icon><span>菜单</span>
         </template>
         <el-menu-item index="2-1">选项？</el-menu-item>
       </el-sub-menu>
@@ -31,6 +30,20 @@
 </template>
 
 <script setup>
+import {defineProps, toRefs } from 'vue';
+const props = defineProps(['isCollapse'])
+const {isCollapse} = toRefs(props)
 
 
 </script>
+
+
+<style scoped>
+.el-menu {
+border-right: none;
+}
+
+.el-menu--collapse .el-sub-menu__title span{
+  display: none;
+}
+</style>
