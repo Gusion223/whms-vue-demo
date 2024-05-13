3d9d6439f443303723f4f59ebe78994c9a87fe46
Kata Gusion <1511684358@qq.com>
2024-05-13 15:31:02 +0800

    header修改结束

--- src/pages/login/whms-basic-header.vue
+++ src/pages/login/whms-basic-header.vue
@@ -1,25 +1,46 @@
 <template>
     <div class="toolbar">
+      <el-icon style="margin-right: 5px; margin-top:1px" :size="20">
+        <expand></expand>
+      </el-icon>
+      <span style="flex:1; text-align: center; font-size:28px;">欢迎来到商城管理系统</span>
+      <span>王小虎</span>
       <el-dropdown>
-        <el-icon style="margin-right: 8px; margin-top: 1px">
-          <arrowDown></arrowDown>
+        <el-icon style="margin-right: 8px; margin-top: 1px; margin-left: 4px;">
+          <arrow-down></arrow-down>
         </el-icon>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>View</el-dropdown-item>
-            <el-dropdown-item>Add</el-dropdown-item>
-            <el-dropdown-item>Delete</el-dropdown-item>
+            <el-dropdown-item>查看</el-dropdown-item>
+            <el-dropdown-item>添加</el-dropdown-item>
+            <el-dropdown-item>删除</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
-      <span>王小虎</span>
+
     </div>
 </template>
 
 <script setup>
+import { ArrowDown, Expand} from '@element-plus/icons-vue'
+
+
+
+// @click
+// const view = ()=>{
+//   alert("点击了查看按钮")
+// }
+
 
 </script>
 
-<style>
-
+<style scoped>
+.toolbar {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100%;
+  right: 20px;
+  border-bottom: darkgray 2px solid;
+}
 </style>
--- src/pages/login/whms-basic.vue
+++ src/pages/login/whms-basic.vue
@@ -38,8 +38,8 @@ import WHMSasicHeader from './whms-basic-header.vue';
 <style scoped>
 .layout-container-demo .el-header {
 position: relative;
-background-color: var(--el-color-primary-light-7);
-color: var(--el-text-color-primary);
+/* background-color: var(--el-color-primary-light-7); */
+/* color: var(--el-text-color-primary); */
 }
 .layout-container-demo .el-aside {
 color: var(--el-text-color-primary);
@@ -51,12 +51,5 @@ border-right: none;
 }
 .layout-container-demo .el-main {
 padding: 0;
-}
-.layout-container-demo .toolbar {
-display: inline-flex;
-align-items: center;
-justify-content: center;
-height: 100%;
-right: 20px;
 }
 </style>
