c567f90f9f0c020ad47e05aae233ab97dab63da9
Kata Gusion <1511684358@qq.com>
2024-06-17 23:38:35 +0800

    Api customer

---
+++ src/api/coustomer/index.js
@@ -0,0 +1,18 @@
+import http from "@/utils/http";
+import {ApiCommonQuery} from "@/api/serviceApi";
+
+export const ApiGetCustomer = (index, size, cname)=>{
+    let params={cname:cname}
+    return ApiCommonQuery("/customer/page", index, size, params)
+}
+export const ApiAddCustomer = (cname, csex, cage, caddr, cphone)=>{
+    return http.post("/customer/add", {cname:cname, csex:csex, cage:cage, caddr:caddr,cphone:cphone})
+}
+
+export const ApiUpdateCustomer =(cid, cname, csex, cage, caddr, cphone)=>{
+    return http.post("/customer/update", {cid:cid, cname:cname, csex:csex, cage:cage, caddr:caddr, cphone:cphone})
+}
+
+export const ApiDeleteCustomer=(cid)=>{
+    return http.post("/customer/delete", {cid:cid});
+}
--- src/api/purchaseOrder/index.js
+++ src/api/purchaseOrder/index.js
@@ -1,5 +1,6 @@
 // PurchaseOrder相关的接口------------------------------------------
 import http from "@/utils/http";
+import {ApiCommonQuery} from "@/api/serviceApi";
 
 export const ApiAddPurchaseOrderWithDetail = (id, wid, sid, potime, details) => {
     return http.post(
@@ -9,4 +10,18 @@ export const ApiAddPurchaseOrderWithDetail = (id, wid, sid, potime, details) => 
             list: details
         }
     )
+}
+
+export const ApiGetPurchaseOrder=(index, size, podate)=>{
+    return ApiCommonQuery("/purchase-order/page", index, size, {podate:podate})
+}
+
+export const ApiAddPurchaseOrder=(id, wid, sid, potime)=>{
+    return http.post("/purchase-order/add", {id:id, wid:wid, sid:sid, potime:potime, pototalCost:0})
+}
+export const ApiUpdatePurchaseOrder=(poid, id, wid, sid, potime)=>{
+    return http.post("/purchase-order/update", {poid:poid, id:id, wid:wid, sid:sid, potime:potime, pototalCost:null})
+}
+export const ApiDeletePurchaseOrder=(poid)=>{
+    return http.post("/purchase-order/delete", {poid:poid})
 }
