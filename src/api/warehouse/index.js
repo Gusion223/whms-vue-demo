4d0e9d22cde60e39bef6f3db6f13872d893c5b4f
Kata Gusion <1511684358@qq.com>
2024-06-18 17:19:44 +0800

    增加了warehouse的Api

--- src/api/warehouse/index.js
+++ src/api/warehouse/index.js
@@ -1,6 +1,23 @@
 // Warehouse的相关接口-------------------------------------------
 import http from "@/utils/http";
+import {ApiCommonQuery} from "@/api/serviceApi";
 
 export const ApiListWarehouse = () => {
     return http.get("/warehouse/list")
+}
+
+export const ApiGetWarehouse = (index, size)=>{
+    return ApiCommonQuery("/warehouse/page", index, size)
+}
+
+export const ApiAddWarehouse=(wname, waddr)=>{
+    http.post("/warehouse/add", {wname:wname, waddr:waddr})
+}
+
+export const ApiUpdateWarehouse = (wid, wname, waddr)=>{
+    http.post("/warehouse/update", {wid:wid, wname:wname, waddr:waddr})
+}
+
+export const ApiDeleteWarehouse = (wid)=>{
+    http.post("/warehouse/delete", {wid:wid})
 }
