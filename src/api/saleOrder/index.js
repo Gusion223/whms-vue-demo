71e5c6a774907c19ab12592a2b2e28777b780503
Kata Gusion <1511684358@qq.com>
2024-06-18 18:08:57 +0800

    修改了SaleOrder的API

--- src/api/saleOrder/index.js
+++ src/api/saleOrder/index.js
@@ -1,4 +1,5 @@
 import http from "@/utils/http";
+import {ApiCommonQuery} from "@/api/serviceApi";
 
 
 export const ApiAddSaleOrderWithDetail = (id, wid, cid, sotime, details)=>{
@@ -7,4 +8,20 @@ export const ApiAddSaleOrderWithDetail = (id, wid, cid, sotime, details)=>{
         list:details
     }
     return http.post("/sale-order/addOrderWithDetail",data)
+}
+
+export const ApiGetSaleOrder = (index, size)=>{
+    return ApiCommonQuery("/sale-order/page", index, size)
+}
+
+export const ApiAddSaleOrder = (id, wid, cid, sotime)=>{
+    return http.post("/sale-order/add", {id:id, wid:wid, cid:cid, sotime:sotime, sototal:0})
+}
+
+export const ApiUpdateSaleOrder = (soid, id, wid, cid, sotime, sototal)=>{
+    return http.post("/sale-order/update", {soid:soid, id:id, wid:wid, cid:cid, sotime:sotime, sototal:sototal})
+}
+
+export const ApiDeleteSaleOrder = (soid)=>{
+    return http.post("/sale-order/delete", {soid:soid})
 }
