50b1eac688b6c123d1bc39eee407c7175d599e6f
Kata Gusion <1511684358@qq.com>
2024-06-18 19:13:06 +0800

    更新了store-record的Api

--- src/api/storeRecord/index.js
+++ src/api/storeRecord/index.js
@@ -3,3 +3,7 @@ import {ApiCommonQuery} from "@/api/serviceApi";
 export const ApiListStoreRecord = (wid)=>{
     return ApiCommonQuery("/store-record/list-fullInfo", null, null, {wid:wid})
 }
+
+export const ApiGetStoreRecord = (index, size)=>{
+    return ApiCommonQuery("/store-record/page-fullInfo",index, size)
+}
